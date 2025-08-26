dc9be01137f17dff9a35318ae64425e1ed09984a
RAJENDRA <rajendra@nalashaa.net>
2025-08-26 16:20:23 +0530

    initialization project

---
+++ backend/server.js
@@ -0,0 +1,102 @@
+import express from "express";
+import { exec, spawn } from "child_process";
+import fs from "fs";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// 1. List devices
+app.get("/devices", (req, res) => {
+  exec("naps2 console --listdevices --driver sane", (err, stdout, stderr) => {
+    if (err) return res.status(500).send(stderr || err.message);
+    const devices = stdout.split("\n").filter(Boolean);
+    res.json(devices);
+  });
+});
+
+// 2. Scan document
+app.post("/scan", (req, res) => {
+  const { device } = req.body;
+  console.log(device);
+  const outputFile = "scan.pdf";
+  let responded = false;
+  // const command = `naps2 console -o  ${outputFile} --noprofile --driver sane --device '${device}'`;
+  // // const command = `naps2 console -o scan.pdf --noprofile --driver sane --device ${device}`;
+
+  // exec(command, (err, stdout, stderr) => {
+  //   console.log({ err, stdout, stderr });
+  //   if (err)
+  //     return res.status(500).json({
+  //       status: 500,
+  //       err: err,
+  //     });
+
+  //   fs.access(outputFile, fs.constants.F_OK, (accessError) => {
+  //     if (accessError) {
+  //       return res.status(500).json({
+  //         status: 500,
+  //         error: "Scan file was not created.",
+  //       });
+  //     }
+  //     fs.readFile(outputFile, (error, data) => {
+  //       console.log("Error", error, data);
+  //       if (error) return res.status(500).send(error.message);
+  //       const base64Image = data.toString("base64");
+  //       res.json({ imageBase64: base64Image });
+  //       fs.unlink(outputFile, (errorFile) => {
+  //         if (errorFile) console.error("Error deleting scan file:", errorFile);
+  //       });
+  //     });
+  //   });
+
+  //   console.log(fs);
+  // });
+
+  const child = spawn("naps2", [
+    "console",
+    "-o",
+    outputFile,
+    "--noprofile",
+    "--driver",
+    "sane",
+    "--device",
+    device,
+  ]);
+
+  child.stdout.on("data", (data) => console.log(`stdout: ${data}`));
+  child.stderr.on("data", (data) => console.error(`stderr: ${data}`));
+
+  const timer = setTimeout(() => {
+    if (!responded) {
+      responded = true;
+      child.kill("SIGKILL"); // stop scanner process
+      return res.status(500).json({ status: 500, error: "Scan timed out" });
+    }
+  }, 60000);
+
+  child.on("close", (code) => {
+    if (responded) return;
+    clearTimeout(timer); // cancel timeout
+    responded = true;
+    
+    if (code !== 0) {
+      return res.status(500).send("Scan failed");
+    }
+
+    fs.readFile(outputFile, (error, data) => {
+      if (error) return res.status(500).send(error.message);
+
+      const base64Image = data.toString("base64");
+      res.json({ imageBase64: base64Image });
+      fs.unlink(outputFile, () => {});
+
+      fs.unlink(outputFile, (errorFile) => {
+        if (errorFile) console.error("Error deleting scan file:", errorFile);
+      });
+    });
+  });
+});
+
+app.listen(5000, () => console.log("Backend running on port 5000"));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,205 @@
+import React, { useState, useEffect } from "react";
+import searchImage from "../src/assets/searching.gif";
+import scanImage from "../src/assets/scan.gif";
+
+export default function ScannerApp() {
+  const [devices, setDevices] = useState([]);
+  const [selectedDevice, setSelectedDevice] = useState("");
+  const [imageBase64, setImageBase64] = useState(null);
+  const [count, setCount] = useState(0);
+  const [loader, setLoader] = useState(false);
+
+  // Fetch scanner devices from backend
+
+  const getDeviceList = async () => {
+    setLoader(true);
+    try {
+      await fetch("http://localhost:5000/devices")
+        .then((res) => res.json())
+        .then((data) => {
+          setDevices(data);
+          setLoader(false);
+        });
+    } catch (err) {
+      console.error(err);
+      setDevices([]);
+      setLoader(false);
+    }
+  };
+  const startScan = async () => {
+    if (!selectedDevice) return alert("Select a device first!");
+    setLoader(true);
+    let timer = setInterval(() => {
+      setCount((prev) => prev + 1);
+    }, 1000);
+    try {
+      const res = await fetch("http://localhost:5000/scan", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ device: selectedDevice }),
+      });
+      const data = await res.json();
+
+      setLoader(false);
+      setCount(0);
+      clearInterval(timer);
+      if (res.status === 500) {
+        alert("Please scan again.");
+        setImageBase64(null);
+        return;
+      }
+      setImageBase64(data.imageBase64);
+    } catch (err) {
+      console.log(err);
+      setLoader(false);
+      setCount(0);
+      clearInterval(timer);
+      setImageBase64(null);
+      console.log(err);
+      if (err.status === 500) {
+        alert("Please scan again.");
+      }
+    }
+  };
+
+  return (
+    <div style={{ width: "100vw" }} className="flex justify-center">
+      {loader ? (
+        <div
+          style={{
+            background: "white",
+            height: "100vh",
+            width: "100vw",
+            position: "fixed",
+            opacity: "0.98",
+            top: "0",
+          }}
+        >
+          <div
+            className="text-center flex justify-center items-center"
+            style={{
+              height: "100vh",
+              width: "100vw",
+            }}
+          >
+            <div>
+              {devices && devices.length === 0 ? (
+                <>
+                  <div>
+                    <img src={searchImage} />
+                  </div>
+                  <h3 className="text-stone-600 mt-4">getting devices...</h3>
+                </>
+              ) : (
+                ""
+              )}
+              {selectedDevice ? (
+                <>
+                  <div>
+                    <img src={scanImage} />
+                  </div>
+                  <h3 className="text-stone-600 mt-4">Scanning...{count}</h3>
+                </>
+              ) : (
+                ""
+              )}
+            </div>
+            <div></div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+      <div className="p-6 text-center">
+        <h1 className="text-2xl font-bold mb-4 ">NAPS2 SCANNER</h1>
+        {/* <table border="1">
+          <thead>
+            <tr>
+              <td>Windows</td>
+              <td>Linux</td>
+              <td>Mac</td>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>
+                <a href="https://github.com/cyanfish/naps2/releases/download/v8.2.0/naps2-8.2.0-win-x64.exe">
+                  <i className="fa fa-download fa-fw"></i>Download
+                </a>
+              </td>
+              <td>
+                <a href="https://github.com/cyanfish/naps2/releases/download/v8.2.0/naps2-8.2.0-linux-x64.deb">
+                  <i className="fa fa-download fa-fw"></i>Download
+                </a>
+              </td>
+              <td>
+                <a href="https://github.com/cyanfish/naps2/releases/download/v8.2.0/naps2-8.2.0-mac-univ.pkg">
+                  <i className="fa fa-download fa-fw"></i>Download
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table> */}
+
+        {devices && devices.length === 0 ? (
+          <button
+            className="bg-blue-500 w-50 h-10 rounded-2xl"
+            onClick={getDeviceList}
+          >
+            Start
+          </button>
+        ) : (
+          ""
+        )}
+
+        {devices && devices.length ? (
+          <>
+            <div>
+              <select
+                className="block appearance-none w-full bg-white border border-gray-300 text-gray-700 py-2 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+                onChange={(e) => setSelectedDevice(e.target.value)}
+              >
+                <option value="">Select Device</option>
+                {devices.map((d, i) => (
+                  <option key={i} value={d}>
+                    {d}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <button
+              className="bg-blue-600 w-50 h-10 rounded-2xl mt-6"
+              onClick={startScan}
+            >
+              Scan Now
+            </button>
+          </>
+        ) : (
+          ""
+        )}
+
+        {imageBase64 && (
+          <div className="mt-6 p-4">
+            <iframe
+              src={`data:application/pdf;base64,${imageBase64}`}
+              width="100%"
+              height="600px"
+            >
+              <p>
+                Your browser does not support iframes. You can{" "}
+                <a
+                  href={`data:application/pdf;base64,${imageBase64}`}
+                  download="your_document.pdf"
+                >
+                  download the PDF
+                </a>{" "}
+                instead.
+              </p>
+            </iframe>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
